54b711d3ef2b69d3633558f403fd160c036ff7d3
bojan12345 <bojantrifkovic92@gmail.com>
2017-01-08 21:21:34 +0100

    Modify splash screen

--- app/src/main/java/com/example/bojan/projekatpma/splash/Splash.java
+++ app/src/main/java/com/example/bojan/projekatpma/splash/Splash.java
@@ -2,10 +2,19 @@ package com.example.bojan.projekatpma.splash;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.bojan.projekatpma.MapsActivity;
 import com.example.bojan.projekatpma.R;
@@ -15,35 +24,38 @@ import com.example.bojan.projekatpma.R;
  */
 
 public class Splash extends Activity {
+    ProgressBar progressBar;
+    int progressStatus = 0;
+    TextView textView1, textView2;
+    Handler handler = new Handler();
 
     @Override
-    protected void onCreate(Bundle savedInsatnceState){
-        super.onCreate(savedInsatnceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
-
-        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        final Animation animation = AnimationUtils.loadAnimation(getBaseContext(),R.anim.rotate);
-        final Animation animation2 = AnimationUtils.loadAnimation(getBaseContext(),R.anim.abc_fade_out);
-
-        imageView.startAnimation(animation);
-        animation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
-
+        textView2 = (TextView) findViewById(R.id.load_per);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textView2.setText(progressStatus + "%");
+                        }
+                    });
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (progressStatus == 100) {
+                    Intent i = new Intent(Splash.this, MapsActivity.class);
+                    startActivity(i);
+                }
             }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                imageView.startAnimation(animation2);
-                finish();
-                Intent intent = new Intent(getBaseContext(), MapsActivity.class);
-                startActivity(intent);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
+        }).start();
     }
 }
